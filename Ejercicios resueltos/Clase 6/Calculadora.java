8d095df9c598714aacb12e5a0f5002eef6f5fdc4
Juan <juanferveg.90@gmail.com>
2023-04-23 23:46:20 -0300

    Agregado el 6

--- Ejercicios resueltos/Clase 6/Calculadora.java
+++ Ejercicios resueltos/Clase 6/Calculadora.java
@@ -1,3 +1,68 @@
-public class Calculadora {
-    
+import java.util.Scanner;
+
+public class Calculadora{
+
+	public static void main(String[] args) {
+		metodos operaciones = new metodos();
+		Scanner entrada = new Scanner(System.in);
+		int n1, n2, resultado, opcion;
+		boolean op = true;
+		
+		while(op){
+			System.out.println("Menu Calculadora");
+			System.out.println("1 - Sumar");
+			System.out.println("2 - Restar");
+			System.out.println("3 - Multiplicar");
+			System.out.println("4 - Dividir");
+			System.out.println("5 - Salir");
+			opcion = entrada.nextInt();
+
+			if (opcion == 5){
+				System.out.println("Salio");
+			}  else{
+
+				switch (opcion){
+
+					case 1:
+						System.out.println("Ingrese el 1er número..");
+						n1 = entrada.nextInt();
+						System.out.println("Ingrese el 2do número..");
+						n2 = entrada.nextInt();
+						resultado = operaciones.sumar(n1, n2);
+						System.out.println("La suma es: " + resultado);
+						break;
+					case 2:
+						System.out.println("Ingrese el 1er número..");
+						n1 = entrada.nextInt();
+						System.out.println("Ingrese el 2do número..");
+						n2 = entrada.nextInt();
+						resultado = operaciones.restar(n1, n2);
+						System.out.println("La resta es: " + resultado);
+						break;
+					case 3:
+						System.out.println("Ingrese el 1er número..");
+						n1 = entrada.nextInt();
+						System.out.println("Ingrese el 2do número..");
+						n2 = entrada.nextInt();
+						resultado = operaciones.multiplicar(n1, n2);
+						System.out.println("La multip es: " + resultado);
+						break;
+					case 4:
+						System.out.println("Ingrese el 1er número..");
+						n1 = entrada.nextInt();
+						System.out.println("Ingrese el 2do número..");
+						n2 = entrada.nextInt();
+						resultado = operaciones.dividir(n1, n2);
+						System.out.println("La div es: " + resultado);
+						break;
+					default:
+						System.out.println("Opcion invalida...");
+					}
+
+
+				}
+  			}
+
+ 		}
+
 }
---
+++ Ejercicios resueltos/Clase 6/MetodosCalculadora.java
@@ -0,0 +1,32 @@
+public class MetodosCalculadora{
+
+	public int sumar(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 + n2;
+	return resultado;
+
+	}
+
+	public int restar(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 - n2;
+	return resultado;
+
+	}
+
+	public int multiplicar(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 * n2;
+	return resultado;
+
+	}
+
+	public int dividir(int n1, int n2){
+	int resultado = 0;
+	resultado = n1 / n2;
+	return resultado;
+
+	}
+
+
+}
