dda37a6227ad7c27dcebcc105c1a0ac50ddbca3d
Juan <juanferveg.90@gmail.com>
2023-04-26 16:13:31 -0300

    Agregado el 7

--- Ejercicios resueltos/Clase 7/Producto.java
+++ Ejercicios resueltos/Clase 7/Producto.java
@@ -1,80 +1,36 @@
-public class Producto {
-    private String nombre;
-    private double precio;
+class Producto {  
+    String ProdNombre;  
+    int ProdCantidad;    
+    double ProdPrecio;
 
-    public Producto(String nombre, double precio) {
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public double getPrecio() {
-        return precio;
+      
+    public Producto(String ProdNombre, int ProdCantidad, double ProdPrecio){  
+        this.ProdNombre = ProdNombre; 
+        this.ProdCantidad= ProdCantidad; 
+        this.ProdPrecio = ProdPrecio;
     }
 }
 
-public class ItemCarrito extends Producto {
-    private int cantidad;
+class itemCarrito extends Producto{   
+    double ItemCPrecTotal; 
+    
+        public itemCarrito(String ProdNombre, int ProdCantidad, double ProdPrecio, double ItemCPrecTotal){
 
-    public ItemCarrito(Producto producto, int cantidad) {
-        super(producto.getNombre(), producto.getPrecio());
-        this.cantidad = cantidad;
+        super(ProdNombre, ProdCantidad, ProdPrecio);  
+        this.ItemCPrecTotal =  ItemCPrecTotal;  		   
     }
 
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public double getSubtotal() {
-        return getCantidad() * getPrecio();
-    }
 }
 
-public class Carrito {
-    private ArrayList<ItemCarrito> items;
 
-    public Carrito() {
-        items = new ArrayList<ItemCarrito>();
+class Descuento extends itemCarrito { 
+    double DescuentoDesc;  
+
+   
+    public Descuento(String ProdNombre, int ProdCantidad, double ProdPrecio, double ItemCPrecTotal, double DescuentoDesc){
+        super(ProdNombre,ProdCantidad,ProdPrecio,ItemCPrecTotal);
+        this.DescuentoDesc = DescuentoDesc;
     }
 
-    public void agregarItem(Producto producto, int cantidad) {
-        ItemCarrito item = new ItemCarrito(producto, cantidad);
-        items.add(item);
-    }
 
-    public void eliminarItem(ItemCarrito item) {
-        items.remove(item);
-    }
-
-    public ArrayList<ItemCarrito> getItems() {
-        return items;
-    }
-
-    public double getTotal() {
-        double total = 0;
-        for (ItemCarrito item : items) {
-            total += item.getSubtotal();
-        }
-        return total;
-    }
 }
-
-public class Descuento {
-    private double porcentaje;
-
-    public Descuento(double porcentaje) {
-        this.porcentaje = porcentaje;
-    }
-
-    public double getPorcentaje() {
-        return porcentaje;
-    }
-
-    public double aplicarDescuento(double total) {
-        return total * (1 - getPorcentaje());
-    }
-}
-
